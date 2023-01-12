1a289c4226228b4960f33bf53eb19b7dcb907f8f
pravash1990 <pravash1990@live.com>
2023-01-12 22:52:55 +0530

    Add project files.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            var userlist = await _context.users.ToListAsync();
+            return userlist;
+        }
+
+        [HttpGet("{id}")]
+        public async Task <ActionResult<AppUser>> GetUser(int id)
+        { 
+            return await _context.users.FindAsync(id);
+        }
+    }
+}
